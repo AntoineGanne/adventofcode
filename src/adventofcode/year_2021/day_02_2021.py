a69153bb83b515e699358799e51002a5e8fbb962
AntoineGanne <antoine.ganne@hotmail.fr>
2021-12-03 13:00:28 +0100

    2021-day 2 and 3

---
+++ src/adventofcode/year_2021/day_02_2021.py
@@ -0,0 +1,59 @@
+from typing import List
+
+from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.util.helpers import solution_timer
+from adventofcode.util.input_helpers import get_input_for_day
+
+
+@solution_timer(2021, 2, 1)
+def part_one(input_data: List[str]):
+    depth = 0
+    horizontal_position = 0
+    for line in input_data:
+        instruction, value = line.split(" ")
+        value = int(value)
+        if instruction == "forward":
+            horizontal_position += value
+        elif instruction == "down":
+            depth += value
+        elif instruction == "up":
+            depth -= value
+        else:
+            raise Exception("error when parsing input")
+    answer = depth*horizontal_position
+
+    if not answer:
+        raise SolutionNotFoundException(2021, 2, 1)
+
+    return answer
+
+
+@solution_timer(2021, 2, 2)
+def part_two(input_data: List[str]):
+    aim=0
+    depth = 0
+    horizontal_position = 0
+    for line in input_data:
+        instruction, value = line.split(" ")
+        value = int(value)
+        if instruction == "forward":
+            horizontal_position += value
+            depth += aim*value
+        elif instruction == "down":
+            aim += value
+        elif instruction == "up":
+            aim -= value
+        else:
+            raise Exception("error when parsing input")
+    answer = depth * horizontal_position
+
+    if not answer:
+        raise SolutionNotFoundException(2021, 2, 2)
+
+    return answer
+
+
+if __name__ == '__main__':
+    data = get_input_for_day(2021, 2)
+    part_one(data)
+    part_two(data)
