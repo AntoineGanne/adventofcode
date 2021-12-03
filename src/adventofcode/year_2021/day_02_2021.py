03eb6911264739fb263e24c0d113a815411eab61
AntoineGanne <antoine.ganne@hotmail.fr>
2021-12-03 13:00:28 +0100

    2021-day 2 and 3

--- src/adventofcode/year_2021/day_02_2021.py
+++ src/adventofcode/year_2021/day_02_2021.py
@@ -4,83 +4,23 @@ from adventofcode.util.exceptions import SolutionNotFoundException
 from adventofcode.util.helpers import solution_timer
 from adventofcode.util.input_helpers import get_input_for_day
 
-Instruction = tuple[str, int]
-Position = tuple[int, int]
-
-FORWARD = 'forward'
-DOWN = 'down'
-UP = 'up'
-
-
-def parse_instruction(instruction: str) -> Instruction:
-    direction, steps = instruction.split(' ')
-    return direction, int(steps)
-
-
-def parse_instructions(input_data: List[str]) -> List[Instruction]:
-    instructions: List[Instruction] = []
-
-    for instruction in input_data:
-        instructions.append(parse_instruction(instruction))
-
-    return instructions
-
-
-def _get_new_position(position: Position, instruction: Instruction) -> Position:
-    x, y = position
-    direction, steps = instruction
-
-    if direction == FORWARD:
-        x += steps
-    elif direction == UP:
-        y -= steps
-    elif direction == DOWN:
-        y += steps
-    else:
-        raise ValueError(f'unknown direction received: {direction}')
-
-    return x, y
-
-
-def _get_new_position_with_aim(position: Position, aim: int, instruction: Instruction) -> tuple[Position, int]:
-    x, y = position
-    direction, steps = instruction
-
-    if direction == FORWARD:
-        x += steps
-        y += aim * steps
-    elif direction == UP:
-        aim -= steps
-    elif direction == DOWN:
-        aim += steps
-    else:
-        raise ValueError(f'unknown direction received: {direction}')
-
-    return (x, y), aim
-
-
-def get_new_position(position: Position, aim: int, instruction: Instruction, include_aim: bool) -> tuple[Position, int]:
-    if not include_aim:
-        return _get_new_position(position, instruction), 0
-
-    return _get_new_position_with_aim(position, aim, instruction)
-
-
-def move_submarine(input_data: List[str], include_aim: bool) -> Position:
-    position = (0, 0)
-    aim = 0
-    instructions = parse_instructions(input_data)
-
-    for instruction in instructions:
-        position, aim = get_new_position(position, aim, instruction, include_aim)
-
-    return position
-
 
 @solution_timer(2021, 2, 1)
 def part_one(input_data: List[str]):
-    x, y = move_submarine(input_data, False)
-    answer = x * y
+    depth = 0
+    horizontal_position = 0
+    for line in input_data:
+        instruction, value = line.split(" ")
+        value = int(value)
+        if instruction == "forward":
+            horizontal_position += value
+        elif instruction == "down":
+            depth += value
+        elif instruction == "up":
+            depth -= value
+        else:
+            raise Exception("error when parsing input")
+    answer = depth*horizontal_position
 
     if not answer:
         raise SolutionNotFoundException(2021, 2, 1)
@@ -90,8 +30,22 @@ def part_one(input_data: List[str]):
 
 @solution_timer(2021, 2, 2)
 def part_two(input_data: List[str]):
-    x, y = move_submarine(input_data, True)
-    answer = x * y
+    aim=0
+    depth = 0
+    horizontal_position = 0
+    for line in input_data:
+        instruction, value = line.split(" ")
+        value = int(value)
+        if instruction == "forward":
+            horizontal_position += value
+            depth += aim*value
+        elif instruction == "down":
+            aim += value
+        elif instruction == "up":
+            aim -= value
+        else:
+            raise Exception("error when parsing input")
+    answer = depth * horizontal_position
 
     if not answer:
         raise SolutionNotFoundException(2021, 2, 2)
--- src/adventofcode/year_2021/day_03_2021.py
+++ src/adventofcode/year_2021/day_03_2021.py
@@ -1,4 +1,3 @@
-from collections import Counter
 from typing import List
 
 from adventofcode.util.exceptions import SolutionNotFoundException
@@ -6,42 +5,29 @@ from adventofcode.util.helpers import solution_timer
 from adventofcode.util.input_helpers import get_input_for_day
 
 
-def input_to_list_of_list(input_data: List[str]) -> List[List[str]]:
-    lists: List[List[str]] = []
-
-    for line in input_data:
-        lists.append([num for num in line])
-
-    return lists
-
-
-def merge_lists(lists: List[List[str]]) -> list[tuple[str]]:
-    merged = zip(*lists)
-    return list(merged)  # type: ignore
-
-
-def get_power_consumption(values: list[tuple[str]]) -> int:
-    gamma_rate = ''
-    epsilon_rate = ''
-
-    for value in values:
-        counter = Counter(value)
-        gamma_rate += str(counter.most_common()[0][0])
-        epsilon_rate += str(counter.most_common()[1][0])
-
-    return int(gamma_rate, 2) * int(epsilon_rate, 2)
-
-
-def get_life_support_rating(values: list[tuple[str]]):
-    ...
-
-
 @solution_timer(2021, 3, 1)
 def part_one(input_data: List[str]):
-    lists = input_to_list_of_list(input_data)
-    merged = list(merge_lists(lists))
-    get_power_consumption(merged)
-    answer = get_power_consumption(merged)
+    weights = [0 for col in range(12)]
+    counter_lines = 0
+    gamma_rate = ['0' for col in range(12)]
+    epsilon_rate = ['0' for col in range(12)]
+    for line in input_data:
+        for i, c in enumerate(line):
+            if c == '0' or c == '1':
+                weights[i] += int(c)
+        counter_lines += 1
+
+    for i in range(len(weights)):
+        if weights[i] > (len(input_data) / 2):
+            gamma_rate[i] = '1'
+            epsilon_rate[i] = '0'
+        else:
+            gamma_rate[i] = '0'
+            epsilon_rate[i] = '1'
+
+    gamma_rate_str = "".join(gamma_rate)
+    epsilon_rate_str = "".join(epsilon_rate)
+    answer = int(gamma_rate_str, 2) * int(epsilon_rate_str, 2)
 
     if not answer:
         raise SolutionNotFoundException(2021, 3, 1)
@@ -49,15 +35,65 @@ def part_one(input_data: List[str]):
     return answer
 
 
+def recursion_part_2(bit_index: int, group_of_binary_numbers: List[str], most_common_bit_mode: bool):
+    print(most_common_bit_mode, ", ", bit_index, ", ", group_of_binary_numbers)
+    if group_of_binary_numbers is None or len(group_of_binary_numbers) == 0:
+        return 0
+    if len(group_of_binary_numbers) == 1:
+        return group_of_binary_numbers[0]
+    groups = [list(), list()]
+    counter_of_ones = 0
+    for binary_number in group_of_binary_numbers:
+        bit = int(binary_number[bit_index])
+        groups[bit].append(binary_number)
+        counter_of_ones += bit
+
+    bit_index += 1
+    if counter_of_ones >= len(group_of_binary_numbers) / 2:
+        most_common_bit = 1
+        least_common_bit = 0
+    else:
+        most_common_bit = 0
+        least_common_bit = 1
+
+    if most_common_bit_mode:
+        group = groups[most_common_bit]
+    else:
+        group = groups[least_common_bit]
+
+    return recursion_part_2(bit_index, group, most_common_bit_mode)
+
+
 @solution_timer(2021, 3, 2)
 def part_two(input_data: List[str]):
-    answer = ...
+    oxygen_generator_rating = recursion_part_2(0, input_data, True)
+    CO2_scrubber_rating = recursion_part_2(0, input_data, False)
+    print(oxygen_generator_rating, ";", CO2_scrubber_rating)
+    print(int(oxygen_generator_rating, 2) ," ", int(CO2_scrubber_rating, 2))
+    answer = int(oxygen_generator_rating, 2) * int(CO2_scrubber_rating, 2)
 
     if not answer:
         raise SolutionNotFoundException(2021, 3, 2)
 
     return answer
 
+@solution_timer(2021,3,3)
+def part_two_with_binary_tree(input_data: List[str]):
+    bin
+
+
+test_data = {"00100",
+             "11110",
+             "10110",
+             "10111",
+             "10101",
+             "01111",
+             "00111",
+             "11100",
+             "10000",
+             "11001",
+             "00010",
+             "01010"}
 
 if __name__ == '__main__':
     data = get_input_for_day(2021, 3)
