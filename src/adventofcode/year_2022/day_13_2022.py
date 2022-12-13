5b9003f27449231a164e676a408ae0589276b4d6
AntoineGanne <antoine.ganne@hotmail.fr>
2022-12-13 21:23:44 +0100

    2022 day 13

---
+++ src/adventofcode/year_2022/day_13_2022.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+import functools
+import math
+import sys
+from functools import cmp_to_key
+from typing import List
+
+from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.util.helpers import solution_timer
+from adventofcode.util.input_helpers import get_input_for_day
+
+
+# -1: invalid , 0: can't say , 1: is_valid
+def is_in_right_order(first: List[int] | int, second: List[int] | int) -> int:
+    if type(first) != type(second):
+        return is_in_right_order([first], second) if type(first) == int else is_in_right_order(first, [second])
+
+    if type(first) == int:
+        if first == second:
+            return 0
+        else:
+            return 1 if first < second else -1
+    else:
+        n, m = len(first), len(second)
+        if n == m == 0: return 0
+        if n == 0: return 1
+        if m == 0: return -1
+        rest = is_in_right_order(first[0], second[0])
+    return rest if rest != 0 else is_in_right_order(first[1:], second[1:])
+
+
+@solution_timer(2022, 13, 1)
+def part_one(input_data: List[str]):
+    nb_pairs = len(input_data) // 3
+    answer = 0
+    for i in range(0, len(input_data), 3):
+        first, second = eval(input_data[i]), eval(input_data[i + 1])
+        index = (i // 3) + 1
+        if is_in_right_order(first, second) == 1:
+            answer += index
+
+    if not answer:
+        raise SolutionNotFoundException(2022, 13, 1)
+
+    return answer
+
+
+def compare(a, b):
+    return -is_in_right_order(a, b)
+
+
+@solution_timer(2022, 13, 2)
+def part_two(input_data: List[str]):
+    packets = []
+    for packet in input_data:
+        if packet == "": continue
+        packets.append(eval(packet))
+    packets.extend([[[2]], [[6]]])
+    packets = sorted(packets, key=cmp_to_key(compare))
+    answer = math.prod([packets.index([[2]]) + 1, packets.index([[6]]) + 1])
+
+    if not answer:
+        raise SolutionNotFoundException(2022, 13, 2)
+
+    return answer
+
+
+if __name__ == '__main__':
+    data = get_input_for_day(2022, 13)
+    part_one(data)
+    part_two(data)
---
+++ tests/year_2022/test_day_13_2022.py
@@ -0,0 +1,16 @@
+from adventofcode.util.input_helpers import get_test_input_for_day
+from adventofcode.year_2022.day_13_2022 import part_two, part_one
+
+
+def test_part_one():
+    assert eval("[9]") == [9]
+    assert eval("[[9,8],7]") == [[9, 8], 7]
+    assert list(zip([], [1])) == []
+    assert list(zip([1], [])) == []
+    answer = part_one(get_test_input_for_day(2022, 13))
+    assert answer == 13
+
+
+def test_part_two():
+    answer = part_two(get_test_input_for_day(2022, 13))
+    assert answer ==140
