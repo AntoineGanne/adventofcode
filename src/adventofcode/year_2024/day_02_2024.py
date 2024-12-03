06eb346917e2231e571e99ec86c9cf8da6358820
AntoineGanne <antoine.ganne@hotmail.fr>
2024-12-03 23:07:19 +0100

    starting 2024

---
+++ src/adventofcode/year_2024/day_02_2024.py
@@ -0,0 +1,31 @@
+from typing import List
+
+from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.util.helpers import solution_timer
+from adventofcode.util.input_helpers import get_input_for_day
+
+
+@solution_timer(2024, 2, 1)
+def part_one(input_data: List[str]):
+    answer = ...
+
+    if not answer:
+        raise SolutionNotFoundException(2024, 2, 1)
+
+    return answer
+
+
+@solution_timer(2024, 2, 2)
+def part_two(input_data: List[str]):
+    answer = ...
+
+    if not answer:
+        raise SolutionNotFoundException(2024, 2, 2)
+
+    return answer
+
+
+if __name__ == '__main__':
+    data = get_input_for_day(2024, 2)
+    part_one(data)
+    part_two(data)
---
+++ tests/year_2024/test_day_02_2024.py
@@ -0,0 +1,12 @@
+from adventofcode.util.input_helpers import get_test_input_for_day
+from adventofcode.year_2024.day_02_2024 import part_two, part_one
+
+
+def test_part_one():
+    answer = part_one(get_test_input_for_day(2024, 2))
+    assert answer is not None
+
+
+def test_part_two():
+    answer = part_two(get_test_input_for_day(2024, 2))
+    assert answer is not None
